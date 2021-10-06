fdb932b9102a42e3ec50ab02c12a77c9148ac1b1
Luis Thomas <luis.thomas2005@gmail.com>
2021-10-06 16:32:02 +0200

    now copy_scripts.sh should be fixed

--- copy_scripts.sh
+++ copy_scripts.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
 # small script to copy my scripts : calls project.sh to generate the files
+
+project_dir="${HOME}/cours_shell" #no space before or after the equal character
+
 sh scripts/project.sh
 
-cp "$(pwd)"/scripts/* scripts/.
+cp "$(pwd)"/scripts/* "${project_dir}"scripts/.
+
